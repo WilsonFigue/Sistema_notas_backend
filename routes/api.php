1efe77ad7b7f75dd5b3961ee4df787622ebf5c21
WilsonFigue <rwillfigueroa@gmail.com>
2024-10-27 20:54:05 -0600

    Cambios para la integración de la funcionalidad completa de las rutas API´s

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/users', function () {
+    $users = User::all();
+    
+    return response()->json($users);
+});
