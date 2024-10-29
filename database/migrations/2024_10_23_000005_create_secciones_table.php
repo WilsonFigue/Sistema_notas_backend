f780bb2069c02ec8df09afc69d80c0705807f63c
WilsonFigue <rwillfigueroa@gmail.com>
2024-10-29 08:41:59 -0600

    Anexo de migración y modelo de la tabla Alumnos, donde se insertan las secciones ya establecidas por medio de migraciones

--- database/migrations/2024_10_23_000005_create_secciones_table.php
+++ database/migrations/2024_10_23_000005_create_secciones_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class CreateSeccionesTable extends Migration
 {
@@ -19,6 +20,17 @@ class CreateSeccionesTable extends Migration
             $table->unsignedSmallInteger('capacidad_seccion'); 
             $table->timestamps(); 
         });
+
+        DB::table('secciones')->insert([
+            ['id_seccion' => 1, 'nombre_seccion' => 'Primero', 'capacidad_seccion' => 90, 'created_at' => now(), 'updated_at' => now()],
+            ['id_seccion' => 3, 'nombre_seccion' => 'Tercero', 'capacidad_seccion' => 90, 'created_at' => now(), 'updated_at' => now()],
+            ['id_seccion' => 4, 'nombre_seccion' => 'Cuarto', 'capacidad_seccion' => 90, 'created_at' => now(), 'updated_at' => now()],
+            ['id_seccion' => 5, 'nombre_seccion' => 'Quinto', 'capacidad_seccion' => 90, 'created_at' => now(), 'updated_at' => now()],
+            ['id_seccion' => 6, 'nombre_seccion' => 'Sexto', 'capacidad_seccion' => 90, 'created_at' => now(), 'updated_at' => now()],
+            ['id_seccion' => 7, 'nombre_seccion' => 'Septimo', 'capacidad_seccion' => 90, 'created_at' => now(), 'updated_at' => now()],
+            ['id_seccion' => 8, 'nombre_seccion' => 'Octavo', 'capacidad_seccion' => 90, 'created_at' => now(), 'updated_at' => now()],
+            ['id_seccion' => 9, 'nombre_seccion' => 'Noveno', 'capacidad_seccion' => 90, 'created_at' => now(), 'updated_at' => now()],
+        ]);
     }
 
     /**
