55e5c69218e674ae353ed49e9913d473d8075809
WilsonFigue <rwillfigueroa@gmail.com>
2024-11-01 21:41:16 -0600

    Modelo de tabla encargado

--- app/Http/Controllers/EncargadoController.php
+++ app/Http/Controllers/EncargadoController.php
@@ -1,10 +1,37 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Models;
 
-use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class EncargadoController extends Controller
+class Encargado extends Model
 {
-    //
+    use HasFactory;
+
+    // Definimos la tabla asociada a este modelo
+    protected $table = 'encargados';
+
+    // La clave primaria personalizada de la tabla
+    protected $primaryKey = 'id_encargado';
+
+    // Especificamos los campos que pueden ser llenados de forma masiva
+    protected $fillable = [
+        'nombre_encargado',
+        'apellido_encargado',
+        'telefono_encargado',
+        'email_encargado',
+        'direccion_encargado',
+        'relacion_estudiante',
+        'DUI_encargado'
+    ];
+
+    // Si no usas incrementing en la clave primaria
+    public $incrementing = true;
+
+    // Si la clave primaria no es un integer
+    protected $keyType = 'int';
+
+    // Para habilitar las marcas de tiempo
+    public $timestamps = true;
 }
