d9f6c8af0798efc8a681bf31d4a559891e597033
WilsonFigue <rwillfigueroa@gmail.com>
2024-11-04 19:35:25 -0600

    Anexo de campo de rol en tabla de Users, ajustando migración, modelo y creación de controlador

--- database/migrations/2024_01_01_000001_create_users_table.php
+++ database/migrations/2024_01_01_000001_create_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->string('id_user', 15)->primary(); 
             $table->string('name_user');
+            $table->string('rol', 20);
             $table->string('email_user')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
