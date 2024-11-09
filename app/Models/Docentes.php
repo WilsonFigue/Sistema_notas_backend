5d535971de8fe3d05a29d1f6f8a3a1e9e405f6b0
WilsonFigue <rwillfigueroa@gmail.com>
2024-11-09 14:55:17 -0600

    Nexo de crud para docentes, añadiendo validaciones y funciones a tablas como users, alumnos y docentes que modifican su funcionalidad para validar todas sus métodos get _ post

--- app/Models/Docentes.php
+++ app/Models/Docentes.php
@@ -2,9 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Docentes extends Model
 {
-    //
+    use HasFactory;
+    protected $table = 'docentes';
+    protected $primaryKey = 'id_docente';
+    public $timestamps = true;
+
+    protected $fillable = [
+        'nombre_docente',
+        'apellido_docente',
+        'correo_docente',
+        'especialidad',
+        'telefono_docente',
+        'direccion_docente',
+        'observaciones_docen',
+        'id_user',
+    ];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id_user');
+    }
 }
